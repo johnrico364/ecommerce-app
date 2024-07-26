3b448e5e47b8b2c10b8badcc2fc4660761e9d5c0
John Anthony Rico <johnrico364@gmail.com>
2024-07-26 15:26:39 +0800

    cart design adjusted

--- client/src/components/CartedProducts.tsx
+++ client/src/components/CartedProducts.tsx
@@ -1,19 +1,18 @@
 export const CartedProducts = () => {
   return (
     <div className="carted-products">
-      <div className="card card-side bg-base-100 h-48">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-            alt="Movie"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <div className="details">
-            <div>Price: P 304</div>
-            <div>Quantity: x2</div>
-            <div className="text-end mt-4">Total: P 608</div>
+      <div className="card card-side bg-base-100 h-44">
+        <div className="sm:basis-4/12 basis-6/12">
+          <img src={require("../images/user/1719302861288.jpg")} alt="product" />
+        </div>
+        <div className="basis-8/12">
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <div className="details">
+              <div>Price: P 304</div>
+              <div>Quantity: x2</div>
+              <div className="text-end mt-4">Total: ₱ 608</div>
+            </div>
           </div>
         </div>
       </div>
--- client/src/pages/user/Cart.tsx
+++ client/src/pages/user/Cart.tsx
@@ -8,23 +8,24 @@ export const Cart = () => {
           <div className="basis-10/12 lg:px-10 mb-5">
             <CartedProducts />
           </div>
-          <div className="basis-2/12 text-end lg:pe-5 pt-5">
+          <div className="basis-1/12 text-end lg:pe-5 pt-5">
             <input className="checkbox" type="checkbox" />
           </div>
 
           <div className="basis-10/12 lg:px-10 mb-5">
             <CartedProducts />
           </div>
-          <div className="basis-2/12 text-end lg:pe-5 pt-5">
+          <div className="basis-1/12 text-end lg:pe-5 pt-5">
             <input className="checkbox" type="checkbox" />
           </div>
-          
         </div>
       </div>
-      
+
       <div className="payment-container flex items-center">
-        <div className="basis-1/2">Payment: P 3234</div>
-        <div className="basis-1/2 text-end lg:pe-5 pe-2"> <button className="button">Check Out</button></div>
+        <div className="basis-1/2">Payment: ₱ 3234</div>
+        <div className="basis-1/2 text-end lg:pe-5 pe-2">
+          <button className="button">Check Out</button>
+        </div>
       </div>
     </div>
   );
