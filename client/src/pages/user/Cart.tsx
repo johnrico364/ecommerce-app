f8989252110d5cfc9dd738d8bca956ad67c9e02a
John Anthony Rico <johnrico364@gmail.com>
2024-08-09 16:42:26 +0800

    dynamic cart datas feature

--- client/src/pages/user/Cart.tsx
+++ client/src/pages/user/Cart.tsx
@@ -1,33 +1,43 @@
 import { useQuery } from "@tanstack/react-query";
 import { CartedProducts } from "../../components/CartedProducts";
 import { useGetAllCarts } from "../../hooks/order/useGetAllCarts";
+import { useParseToken } from "../../hooks/user/useParseToken";
+import { useState } from "react";
 
 export const Cart = () => {
   const { getAllCarts } = useGetAllCarts();
+  const { parseToken } = useParseToken();
 
-  useQuery({ queryKey: ["carts"] ,
-    queryFn: async ()=> {
-      // await getAllCarts()
-    }
+  const [cartData, set_cartData] = useState<any>([]);
+
+  const effectCart = async () => {
+    const userData = await parseToken();
+    const carts = await getAllCarts(userData?._id);
+    set_cartData(carts);
+
+    return true;
+  };
+
+  useQuery({
+    queryKey: ["carts"],
+    queryFn: effectCart,
   });
   return (
     <div className="cart-container">
       <div className="product-container">
-        <div className="flex flex-wrap">
-          <div className="basis-10/12 lg:px-10 mb-5">
-            <CartedProducts />
-          </div>
-          <div className="basis-1/12 text-end lg:pe-5 pt-5">
-            <input className="checkbox" type="checkbox" />
-          </div>
-
-          <div className="basis-10/12 lg:px-10 mb-5">
-            <CartedProducts />
-          </div>
-          <div className="basis-1/12 text-end lg:pe-5 pt-5">
-            <input className="checkbox" type="checkbox" />
-          </div>
-        </div>
+        {cartData?.map((cart: any) => {
+          console.log(cart)
+          return (
+            <div className="flex flex-wrap">
+              <div className="basis-10/12 lg:px-10 mb-5">
+                <CartedProducts data={cart}/>
+              </div>
+              <div className="basis-1/12 text-end lg:pe-5 pt-5">
+                <input className="checkbox" type="checkbox" />
+              </div>
+            </div>
+          );
+        })}
       </div>
 
       <div className="payment-container flex items-center">
