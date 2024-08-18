dff48672ff05fa2527052b373cdb82297dd8b55e
John Anthony Rico <johnrico364@gmail.com>
2024-08-18 23:54:38 +0800

    Connect The Mongo DB to Node JS

--- server/server.js
+++ server/server.js
@@ -9,8 +9,10 @@ const orderRoutes = require("./routes/order");
 const app = express();
 app.use(express.json());
 
-const _dbURI =
-  "mongodb+srv://johnrico:john2004@projectsystems.cg2cala.mongodb.net/information_management?retryWrites=true&w=majority&appName=ProjectSystems";
+// const _dbURI =
+//   "mongodb+srv://johnrico:john2004@projectsystems.cg2cala.mongodb.net/information_management?retryWrites=true&w=majority&appName=ProjectSystems";
+
+const _dbURI = "mongodb://localhost:27017/saylava";
 
 mongoose.connect(_dbURI).then(() => {
   console.log("Connected");
