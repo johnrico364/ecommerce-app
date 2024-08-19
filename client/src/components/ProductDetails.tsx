fde317936a9554c2888357467c33d3571a8ffdfa
John Anthony Rico <johnrico364@gmail.com>
2024-08-19 11:44:24 +0800

    Modify Admin Prodcuts Details Card

--- client/src/components/ProductDetails.tsx
+++ client/src/components/ProductDetails.tsx
@@ -19,14 +19,14 @@ export const ProductDetails: React.FC<ProductDetailsProps> = ({ data }) => {
   return (
     <div className="product-details mb-3 me-4 overflo-auto">
       <div className="card card-side h-42 bg-base-100">
-        <div className="basis-3/12">
+        <div className="basis-4/12">
           <img
             src={require(`../images/product/${data.picture}`)}
             alt="Product"
           />
         </div>
-        <div className="basis-9/12">
-          <div className="card-body">
+        <div className="basis-8/12">
+          <div className="card-body gap-1">
             <h2 className="card-title">{data.name}</h2>
             <div className="details">{data.description}</div>
             <div className="details">Stocks: {data.stocks}</div>
