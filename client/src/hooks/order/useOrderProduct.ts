dd5d599c716756a13faa1d25dd79c5725abc6366
John Anthony Rico <johnrico364@gmail.com>
2024-08-19 15:52:02 +0800

    alert api response

--- client/src/hooks/order/useOrderProduct.ts
+++ client/src/hooks/order/useOrderProduct.ts
@@ -5,9 +5,11 @@ export const useOrderProduct = () => {
     try {
       const order = await axios.post("/api/order/new-order", newOrder);
       console.log(order?.data);
+      alert(order?.data);
       return { data: order?.data, success: true };
     } catch (error: any) {
       console.log(error?.response?.data);
+      alert(error?.response?.data);
     }
   };
 
