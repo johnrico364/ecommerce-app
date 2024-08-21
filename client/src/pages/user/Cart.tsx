cb45d532eb220201f35129c25392ef15f8fa6203
John Anthony Rico <johnrico364@gmail.com>
2024-08-21 13:17:23 +0800

    check out product feature

--- client/src/pages/user/Cart.tsx
+++ client/src/pages/user/Cart.tsx
@@ -3,12 +3,21 @@ import { CartedProducts } from "../../components/CartedProducts";
 import { useGetAllCarts } from "../../hooks/order/useGetAllCarts";
 import { useParseToken } from "../../hooks/user/useParseToken";
 import { useState } from "react";
+import { useCheckoutCart } from "../../hooks/order/useCheckoutCart";
+import { useNavigate } from "react-router-dom";
+import { FaCircleCheck, FaXmark } from "react-icons/fa6";
 
 export const Cart = () => {
+  const navigate = useNavigate();
+
   const { getAllCarts } = useGetAllCarts();
   const { parseToken } = useParseToken();
+  const { checkoutCart } = useCheckoutCart();
 
   const [cartData, set_cartData] = useState<any>([]);
+  const [checkOut, set_checkOut] = useState<string[]>([]);
+  const [totalPayment, set_totalPayment] = useState<number>(0);
+  const [checkOutMessage, set_checkOutMessage] = useState<string>("");
 
   const effectCart = async () => {
     const userData = await parseToken();
@@ -17,23 +26,61 @@ export const Cart = () => {
 
     return true;
   };
-
   useQuery({
     queryKey: ["carts"],
     queryFn: effectCart,
   });
+
+  const addCheckOutFn = (
+    cartId: string,
+    cartPayment: number,
+    isChecked: boolean
+  ) => {
+    if (isChecked) {
+      set_checkOut((prev: any) => [...prev, cartId]);
+      set_totalPayment(totalPayment + cartPayment);
+    } else {
+      set_checkOut((prev: any) => prev.filter((id: any) => id !== cartId));
+      set_totalPayment(totalPayment - cartPayment);
+    }
+  };
+
+  const toCheckOutFn = async () => {
+    if (checkOut.length > 0) {
+      const response = await checkoutCart(checkOut);
+      response?.status && set_checkOutMessage(response?.response);
+    } else {
+      alert("!! No order selected");
+    }
+  };
+
   return (
     <div className="cart-container">
+      {checkOutMessage !== "" && (
+        <div className="mt-3 mx-3 text-white font-semibold alert alert-success">
+          <FaCircleCheck className="text-2xl" />
+          <span>Your purchase has been confirmed!</span>
+          <button onClick={() => navigate("/user/profile")}>
+            <FaXmark className="text-2xl" />
+          </button>
+        </div>
+      )}
+
       <div className="product-container">
         {cartData?.map((cart: any) => {
-          console.log(cart)
           return (
             <div className="flex flex-wrap">
               <div className="basis-10/12 lg:px-10 mb-5">
-                <CartedProducts data={cart}/>
+                <CartedProducts data={cart} />
               </div>
               <div className="basis-1/12 text-end lg:pe-5 pt-5">
-                <input className="checkbox" type="checkbox" />
+                <input
+                  className="checkbox"
+                  type="checkbox"
+                  onChange={(e) =>
+                    addCheckOutFn(cart?._id, cart?.payment, e.target.checked)
+                  }
+                />
               </div>
             </div>
           );
@@ -41,9 +88,11 @@ export const Cart = () => {
       </div>
 
       <div className="payment-container flex items-center">
-        <div className="basis-1/2">Payment: ₱ 3234</div>
+        <div className="basis-1/2 color-2nd-1">Payment: ₱ {totalPayment}</div>
         <div className="basis-1/2 text-end lg:pe-5 pe-2">
-          <button className="button">Check Out</button>
+          <button className="button" onClick={toCheckOutFn}>
+            Check Out
+          </button>
         </div>
       </div>
     </div>
