2dae4e82a9d55cfed781cd5d662bd76fc5fbd6f1
John Anthony Rico <johnrico364@gmail.com>
2024-08-22 09:03:31 +0800

    make user orders dynamic

--- client/src/components/OrderDetails.tsx
+++ client/src/components/OrderDetails.tsx
@@ -1,20 +1,41 @@
-export const OrderDetails = () => {
+import React from "react";
+
+interface OrderDetailsProps {
+  data: {
+    quantity: number;
+    payment: number;
+    createdAt: string;
+    product: {
+      name: string;
+      description: string;
+      price: number;
+      picture: string;
+    };
+  };
+}
+
+export const OrderDetails: React.FC<OrderDetailsProps> = ({ data }) => {
+  const formatter = new Intl.NumberFormat("en").format;
   return (
     <div className="order-card">
-      <div className="card card-side h-40 bg-base-100 overflow-auto">
-        <figure>
+      <div className="card card-side bg-base-100 h-44">
+        <div className="sm:basis-3/12 basis-6/12">
           <img
-            className="h-40"
-            src={require("../images/assets/Logo_v2.png")}
+            src={require(`../images/product/${data?.product?.picture}`)}
             alt="product"
+            className="h-44"
           />
-        </figure>
-        <div className="card-body py-0 px-3">
-          <div className="card-title">Lactom</div>
-          <div className="details">200ml canned milk powder shaboha yko pina twerk</div>
-          <div className="details">Order By: dq3n312oi12h3o13</div>
-          <div className="text-end">x 2</div>
-          <div className="price">P 3,100</div>
+        </div>
+        <div className="basis-8/12">
+          <div className="card-body gap-1 py-0 px-3">
+            <div className="card-title">{data?.product?.name}</div>
+            <div className="details ">{data?.product?.description}</div>
+            <div className="text-end mt-2">
+              ₱{data?.product?.price} x {data?.quantity}
+            </div>
+            <div className="price">Total: ₱ {formatter(data?.payment)}</div>
+            <div className="details text-end">Ordered 2 Days Ago</div>
+          </div>
         </div>
       </div>
     </div>
--- client/src/pages/user/Profile.tsx
+++ client/src/pages/user/Profile.tsx
@@ -9,8 +9,10 @@ import { useSelector } from "react-redux";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParseToken } from "../../hooks/user/useParseToken";
+import { useGetOrderByStatus } from "../../hooks/order/useGetOrderByStatus";
 
 interface UserData {
+  _id: string;
   address: string;
   fname: string;
   lname: string;
@@ -19,26 +21,30 @@ interface UserData {
 
 export const Profile = () => {
   const navigate = useNavigate();
+
   const { parseToken } = useParseToken();
+  const { getUserOrder } = useGetOrderByStatus();
 
   const [userData, set_userData] = useState<UserData>();
+  const [ordersData, set_ordersData] = useState<string[]>([]);
+  const [queryOrderStatus, set_queryOrderStatus] =
+    useState<string>("to-approve");
 
   const effectProf = async () => {
     try {
       const user = await parseToken();
       set_userData(user);
+
+      const orders = await getUserOrder(user?._id, queryOrderStatus);
+      set_ordersData(orders);
     } catch (error: any) {
       console.log(error);
     }
   };
 
-  const getUserOrders = async (query: string) => {
-    console.log(query);
-  };
-
   useEffect(() => {
     effectProf();
-  }, []);
+  }, [queryOrderStatus]);
 
   const logoutFn = () => {
     localStorage.removeItem("user");
@@ -80,16 +86,22 @@ export const Profile = () => {
           <div className="xl:w-10/12 lg:px-0 w-full px-2  mx-auto">
             <select
               className="border-2 rounded-md"
-              onChange={(e) => getUserOrders(e.target.value)}
+              onChange={(e) => set_queryOrderStatus(e.target.value)}
             >
-              <option selected>To Approve</option>
-              <option>To Ship</option>
-              <option>Purchase History</option>
+              <option value={"to-approve"}>To Approve</option>
+              <option value={"to-ship"}>To Ship</option>
+              <option value={"history"}>Purchase History</option>
             </select>
 
             <div className="card-container flex">
               <div className="basis-full">
-                <OrderDetails />
+                {ordersData?.map((order: any) => {
+                  return (
+                    <div>
+                      <OrderDetails data={order} />
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </div>
