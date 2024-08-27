a571d465955d20014fdd0f74f374cdeba5b0d501
John Anthony Rico <johnrico364@gmail.com>
2024-08-27 23:16:57 +0800

    make user details components dynamic dependent

--- client/src/components/UserDetails.tsx
+++ client/src/components/UserDetails.tsx
@@ -1,23 +1,47 @@
-export const UserDetails = () => {
+import { formatDistanceToNow } from "date-fns";
+import React from "react";
+
+interface UserDetailsProps {
+  data: {
+    fname: string;
+    lname: string;
+    address: string;
+    email: string;
+    picture: string;
+    updatedAt: string;
+  };
+  index: number;
+}
+
+export const UserDetails: React.FC<UserDetailsProps> = ({ data, index }) => {
+  const updateDate = formatDistanceToNow(new Date(data.updatedAt), {
+    addSuffix: true,
+  });
+
   return (
-    <>
-      <div className="font-semibold ps-2"># 1</div>
-      <div>
-        <span className="font-semibold">Name: </span>
-        John Anthony Rico
+    <div className="flex flex-wrap">
+      <div className="basis-4/12 pr-2">
+        <img src={require(`../images/user/${data.picture}`)} alt="" />
       </div>
-      <div>
-        <span className="font-semibold">Address: </span>
-        Mandaue City,Cebu, Philippines
+      <div className="basis-8/12">
+        <div className="font-semibold ps-2"># {index}</div>
+        <div>
+          <span className="font-semibold">Name: </span>
+          {data.lname}, {data.fname}
+        </div>
+        <div>
+          <span className="font-semibold">Address: </span>
+          {data.address}
+        </div>
+        <div>
+          <span className="font-semibold">Email: </span>
+          {data.email}
+        </div>
+        <div>
+          <span className="font-semibold">Updated: </span>
+          {updateDate}
+        </div>
       </div>
-      <div>
-        <span className="font-semibold">Email: </span>
-        johnrico364@gmail.com
-      </div>
-      <div>
-        <span className="font-semibold">Admin: </span>
-        No
-      </div>
-    </>
+    </div>
   );
 };
